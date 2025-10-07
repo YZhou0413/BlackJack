a08f2494d713e2ea7e1fceb7589ce5e42620c2ae
Abigail Major <acmajor260@gmail.com>
2025-10-07 21:18:58 +0200

    Add basic PySide6 application
    Executing user_interface.py opens the app, displaying a basic menu
    defined in menu.py.

---
+++ src/gui/menu.py
@@ -0,0 +1,50 @@
+from PySide6.QtWidgets import (
+    QPushButton, QVBoxLayout, QWidget
+)
+
+
+# represents game menu
+class Menu(QWidget):
+
+    # CONSTRUCTOR
+    def __init__(self):
+        super().__init__()
+
+        #---- create menu buttons and slots ----
+        # create login button
+        login_button = QPushButton("Login")
+        login_button.clicked.connect(self.open_login_view)
+        # create show rules button
+        show_rules_button = QPushButton("Rules")
+        show_rules_button.clicked.connect(self.open_rules_view)
+        # create show scoreboard button
+        show_scoreboard_button = QPushButton("Scoreboard")
+        show_scoreboard_button.clicked.connect(self.open_scoreboard_view)
+        # create app exit button
+        exit_button = QPushButton("Exit")
+        exit_button.clicked.connect(self.exit_game)
+
+
+        #---- create menu layout ----
+        menu_layout = QVBoxLayout()
+        menu_layout.addWidget(login_button)
+        menu_layout.addWidget(show_rules_button)
+        menu_layout.addWidget(show_scoreboard_button)
+        menu_layout.addWidget(exit_button)
+
+        # add menu elements to menu
+        self.setLayout(menu_layout)
+
+
+    # SIGNAL HANDLER METHODS
+    def open_login_view(self):
+        print("Login button pressed")
+
+    def open_rules_view(self):
+        print("Rules button pressed")
+
+    def open_scoreboard_view(self):
+        print("Scoreboard button pressed")
+
+    def exit_game(self):
+        print("Exit game button was pressed")
--- src/gui/user_interface.py
+++ src/gui/user_interface.py
@@ -6,45 +6,54 @@
 # Software Assignment                #
 ######################################
 
-from game import Game
-from player import Player
+from PySide6.QtCore import QSize
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+)
+from menu import Menu
 
-class UI:
-    '''
-    Class for any inputs and outputs.
 
-    User controls must be implemented in this class!
-    '''
+# Represents app window
+class MainWindow(QMainWindow):
 
+    # CLASS CONSTANTS
+    # fixed width and height of main window
+    WINDOW_FIXED_WIDTH = 700
+    WINDOW_FIXED_HEIGHT = int(WINDOW_FIXED_WIDTH * 0.66)
+
+
+    # CONSTRUCTOR
     def __init__(self):
-        '''
-        Creates a new user interface. Think about which pieces of information you need at this time
-        (e.g. players, game mode, time limit, ...) and add them as arguments.
-        '''
-        pass
+        #---- setup main window ----
+        # call constructor method of QMainWindow
+        super().__init__()
 
-    def load_highscore(self, filename):
-        pass
+        # set title of window
+        self.setWindowTitle("Black Jack")
 
-    def add_highscore(self, name, score, scores):
-        pass
+        # set fixed size of the main window
+        self.setFixedSize(QSize(MainWindow.WINDOW_FIXED_WIDTH, MainWindow.WINDOW_FIXED_HEIGHT))
 
-    def save_highscore(self, scores, filename):
-        pass
 
-    def display(self):
-        pass
+        # ---- setup menu ----
+        # create menu page
+        menu = Menu()
 
-    def frame(self):
-        pass
-
-    def wait_for_input(self):
-        pass
+        # set menu as central widget of main window
+        self.setCentralWidget(menu)
 
 
 if __name__ == '__main__':
     '''
     write additional testing code here for things that don't work well as unit tests:
     '''
-    ui = UI()
-    ...
+    # create QApp instance
+    app = QApplication([])
+
+    # create and show main window
+    window = MainWindow()
+    window.show()
+
+    # start event loop
+    app.exec()
