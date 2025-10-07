a90d12f389d1b1a76dd3da7d596e2116b5adba62
Abigail Major <acmajor260@gmail.com>
2025-10-08 00:27:28 +0200

    Add basic login form widget to project

---
+++ src/gui/login.py
@@ -0,0 +1,59 @@
+from PySide6.QtWidgets import (
+    QWidget,
+    QPushButton,
+    QFormLayout,
+    QLineEdit,
+    QApplication,
+    QTextEdit
+)
+
+class Login(QWidget):
+
+    # CONSTRUCTOR
+    def __init__(self):
+        super().__init__()
+
+        #---- create login form ----
+        # create signin button
+        signin_button = QPushButton("Sign in")
+        signin_button.clicked.connect(self.validate_signin)
+
+        # create form info textfield
+        form_info = QTextEdit()
+        form_info.setText("Creates a new account, if the username is not yet taken.")
+        form_info.setReadOnly(True)
+
+
+        #---- create login layout ----
+        form_layout = QFormLayout()
+
+        # display input fields below their respective label
+        form_layout.setRowWrapPolicy(QFormLayout.RowWrapPolicy.WrapAllRows)
+        form_layout.setFieldGrowthPolicy(QFormLayout.FieldGrowthPolicy.AllNonFixedFieldsGrow)
+
+        # add form elements to layout
+        form_layout.addRow("username", QLineEdit("username"))
+        form_layout.addRow("password", QLineEdit("password"))
+        form_layout.addRow(signin_button)
+        form_layout.addRow(form_info)
+
+        # set layout of login widget
+        self.setLayout(form_layout)
+
+    # SIGNAL HANDLER METHODS
+    def validate_signin(self):
+        print("Check if username and password are valid")
+        print("Check if username exist. If it does, compare passwords.")
+        print("If password invalid, show error")
+
+
+if __name__ == "__main__":
+    # create QApp instance
+    app = QApplication([])
+
+    # create and show main window
+    window = Login()
+    window.show()
+
+    # start event loop
+    app.exec()
